d5bc3721e8a6e0465141535e0c83c2fc779b665a
Irpan Budiana <irpan.budiana@gmail.com>
2020-02-11 10:51:38 +0700

    instalasi paket material

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import './App.css';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
+import { Layout, Header, Navigation, Content } from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
-      <div> <hr />
-        <Link to="/home">Home</Link> | 
-        <Link to="/about">About</Link> | 
-        <Link to="/project">Project</Link> | 
-        <Link to="/contact">Contact</Link> <hr />
-
-        <p><Main /></p>
+      <div className="demo-big-content">
+        <Layout>
+          <Header className="header-color" title="Portfolio" scroll>
+            <Navigation>
+              <Link to="/home">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/project">Project</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Content>
+            <div className="page-content" />
+              <Main />
+          </Content>
+        </Layout>
       </div>
-    )
+    );
   }
 }
 
